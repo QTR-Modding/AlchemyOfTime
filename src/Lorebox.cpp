1f695f75fb97a00c5c49574a3f1e246555469831
Ege Yilmaz <47591838+Quantumyilmaz@users.noreply.github.com>
2025-12-13 03:47:37 +0100

    Add null check for item_data in translation request
    Prevents potential null pointer dereference by returning early if item_data is null in Lorebox::OnDynamicTranslationRequest.

--- src/Lorebox.cpp
+++ src/Lorebox.cpp
@@ -450,6 +450,9 @@ std::wstring Lorebox::BuildFrozenLore(const std::wstring& currentStageName) {
 
 const wchar_t* Lorebox::OnDynamicTranslationRequest(std::string_view) {
     const auto item_data = Menu::GetSelectedItemDataInMenu();
+    if (!item_data) {
+        return return_str.c_str();
+    }
     const auto item = item_data->objDesc->GetObject();
     if (!item) {
         return return_str.c_str();
